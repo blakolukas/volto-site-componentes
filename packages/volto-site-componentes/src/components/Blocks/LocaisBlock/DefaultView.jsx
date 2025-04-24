e7cfdd3e95dffd796e403dc706b4926e039f0d95
blakolukas <lucasblacovicz@gmail.com>
2025-04-24 13:44:46 -0300

    #AB1149782 - linter chato

--- packages/volto-site-componentes/src/components/Blocks/LocaisBlock/DefaultView.jsx
+++ packages/volto-site-componentes/src/components/Blocks/LocaisBlock/DefaultView.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container } from 'semantic-ui-react';
-import { UniversalLink } from '@plone/volto/components';
 import Endereco from '../../Endereco/Endereco';
 
 const LocalSummary = ({ content }) => {
-  console.log('content', content);
   return (
     <div className={`Local summary`} style={{ paddingBottom: '40px' }}>
       <Endereco content={content} />
